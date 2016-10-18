28a96a408f7ed437a635fb5f396bc374399633ed
praveenearle <praveen.earle@gmail.com>
2016-10-18 09:33:18 +0530

    java script s1a2
    java script s1a2

---
+++ java script 1assignment 2/java script 1assignment 2/current-time.js
@@ -0,0 +1,10 @@
+﻿function showTime() {
+  var timeNow = new Date();
+  var hours   = timeNow.getHours();
+  var minutes = timeNow.getMinutes();
+  
+  var timeString = "" + ((hours > 12) ? hours - 12 : hours);
+  timeString  += ((minutes < 10) ? ":0" : ":") + minutes;
+  document.htmlClock.timeField.value = timeString;
+  timerID = setTimeout("showTime()", 1000);
+}
